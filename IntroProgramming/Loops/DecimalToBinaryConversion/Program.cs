725056bd74dddc8cd4e4d26243360a4a14b4ddad
Ivan Boykov Peev <ivan.boykov.peev@gmail.com>
2015-09-02 17:33:51 +0300

    Decimal to binary convert program

---
+++ IntroProgramming/Loops/DecimalToBinaryConversion/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DecimalToBinaryConversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int inputNumber = int.Parse(Console.ReadLine());
+            List<int> result = new List<int>();
+
+            if (inputNumber == 0)
+            {
+                Console.WriteLine(0);
+            }
+            else
+            {
+                while (inputNumber > 0)
+                {
+                    int remainder = inputNumber % 2;
+                    inputNumber /= 2;
+                    result.Add(remainder);
+                }
+                //Print list
+                result.Reverse(); //revers list
+                foreach (var item in result)
+                {
+                    Console.Write(item);
+                }
+                Console.WriteLine();
+            }            
+        }
+    }
+}
