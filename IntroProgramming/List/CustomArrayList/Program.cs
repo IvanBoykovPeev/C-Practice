d4bb0de733ffe1c6218436388570bbef46c5ce3e
Ivan Boykov Peev <ivan.boykov.peev@gmail.com>
2015-09-19 21:42:49 +0300

    Implement List.sln

---
+++ IntroProgramming/List/CustomArrayList/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomArrayList
+{
+    class Program
+    {
+        static void Main()
+        {
+            CustomArrayList shoppingList = new CustomArrayList();
+            shoppingList.Add("Milk");
+            shoppingList.Add("Honey");
+            shoppingList.Add("Olives");
+            shoppingList.Add("Beer");
+            shoppingList.Remove("Olives");
+            Console.WriteLine("We need to buy:");
+            for (int i = 0; i < shoppingList.Count; i++)
+            {
+                Console.WriteLine(shoppingList[i]);
+            }
+            Console.WriteLine("Do we have to buy Bread? " +
+                shoppingList.Contains("Bread"));
+        }
+    }
+}
